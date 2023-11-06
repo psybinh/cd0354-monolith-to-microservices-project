039b8042891b30cdf1a89f2e3200b7505c29f9a0
Phung Binh <psybinh@gmail.com>
2023-11-06 23:54:43 +0700

    update link

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a51aeebe775044eacb67918d8ee54606-1574883291.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
